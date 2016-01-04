c6da806a8f2ca9d80cda3a2cbbf46a87650aec32
Michal Fijolek <michalfijolek91@gmail.com>
2016-01-04 21:01:37 +0100

    match lineups refactored, closes #14

--- src/main/scala/soccerstand/parser/MatchLineupsParser.scala
+++ src/main/scala/soccerstand/parser/MatchLineupsParser.scala
@@ -7,6 +7,8 @@ import soccerstand.parser.matchsummary.Common.MatchTeamTag.{AwayTeam, HomeTeam}
 import soccerstand.parser.token.SoccerstandTokens._
 
 import scala.xml.{Elem, Node, NodeSeq}
+import scalaz.Scalaz._
+import scalaz._
 
 object MatchLineupsParser {
   sealed trait TeamMember
@@ -16,116 +18,96 @@ object MatchLineupsParser {
   }
 
   sealed trait MatchTeamMemberTag
+  sealed trait PlayerTeamMemberTag
   object MatchTeamMemberTags {
-    case object StartingLineupPlayerTag extends MatchTeamMemberTag
-    case object PlayerOnTheBenchTag extends MatchTeamMemberTag
+    case object StartingLineupPlayerTag extends MatchTeamMemberTag with PlayerTeamMemberTag
+    case object PlayerOnTheBenchTag extends MatchTeamMemberTag with PlayerTeamMemberTag
     case object CoachTag extends MatchTeamMemberTag
   }
 
   case class MatchLineups(league: League, matchInfo: Match, lineups: Lineups)
   case class Lineups(homeTeamLineup: TeamLineup, awayTeamLineup: TeamLineup)
-  case class TeamLineup(startingLineup: Seq[Player], bench: Seq[Player], coach: Option[Coach])
+  case class TeamLineup(startingLineup: List[Player], bench: List[Player], coach: Option[Coach])
+  case class ParsedTeamMember(matchTeamTag: MatchTeamTag, member: (Player, PlayerTeamMemberTag) \/ Coach)
 
   import soccerstand.implicits.Implicits._
 
   def parseLineups(lineupsAsHtml: Elem): Lineups = {
-    val typedMatchEvents = allTeamMembersTyped(lineupsAsHtml)
-    val homeTeamEvents = typedMatchEvents(MatchTeamTag.HomeTeam)
-    val awayTeamEvents = typedMatchEvents(MatchTeamTag.AwayTeam)
-    val homeTeamMatchEvents = createMatchStageEvents(homeTeamEvents)
-    val awayTeamMatchEvents = createMatchStageEvents(awayTeamEvents)
-    Lineups(homeTeamMatchEvents, awayTeamMatchEvents)
-  }
-  private def createMatchStageEvents(teamEvents: Map[MatchTeamMemberTag, Seq[TeamMember]]): TeamLineup = {
-    /*fixme instanceof*/
-    TeamLineup(
-      startingLineup = teamEvents(MatchTeamMemberTags.StartingLineupPlayerTag).asInstanceOf[Seq[Player]],
-      bench = teamEvents(MatchTeamMemberTags.PlayerOnTheBenchTag).asInstanceOf[Seq[Player]],
-      coach = teamEvents(MatchTeamMemberTags.CoachTag).headOption.asInstanceOf[Option[Coach]]
-    )
-  }
-
-  /*fixme deduplicate MatchEvents*/
-  private def allTeamMembersTyped(matchSummaryAsHtml: Elem): Map[MatchTeamTag, Map[MatchTeamMemberTag, Seq[TeamMember]]] = {
-    val matchMembers = allLineups(matchSummaryAsHtml)
-    val matchMembersByTeam = groupEventsByTeam(matchMembers)
-    matchMembersByTeam.mapValues { makeMembersTyped }.withDefaultValue(Map())
-  }
-
-  private def makeMembersTyped(matchStageWithMembers: Map[MatchTeamMemberTag, NodeSeq]): Map[MatchTeamMemberTag, Seq[TeamMember]] = {
-    val typedMembers = matchStageWithMembers.map { case (stageTag, events) =>
-      stageTag -> makeMembersTyped(events, stageTag)
+    val allLineupsData = (lineupsAsHtml \\ "tr").toVector
+    val zero = (List.empty[ParsedTeamMember], MatchTeamMemberTags.StartingLineupPlayerTag: MatchTeamMemberTag)
+    val (allTeamsMembers, _) = allLineupsData.foldLeft(zero) { case ((teamMembers, previousTag), currentLineupDataLine) =>
+      val (currentTag, isNodeATag) = if ((currentLineupDataLine \@ "class").isEmpty) {
+        (defineNextTeamMembers(currentLineupDataLine), true)
+      } else (previousTag, false)
+      if (!isNodeATag) {
+        (extractMembersFromNode(currentLineupDataLine, currentTag) ::: teamMembers, currentTag)
+      } else (teamMembers, currentTag)
     }
-    typedMembers.withDefaultValue(Seq())
+    val (homeMembers, awayMembers) = allTeamsMembers.partition { parsedMember => parsedMember.matchTeamTag == MatchTeamTag.HomeTeam }
+    Lineups(extractParsedMembers(homeMembers), extractParsedMembers(awayMembers))
   }
 
-  private def makeMembersTyped(members: NodeSeq, matchStageTag: MatchTeamMemberTag): Seq[TeamMember] = {
-    matchStageTag match {
-      case MatchTeamMemberTags.PlayerOnTheBenchTag | MatchTeamMemberTags.StartingLineupPlayerTag =>
-        members.map { node =>
-          val teamMemberDiv = node \ "div"
-          val number = teamMemberDiv.getTextFromClass("time-box")
-          val name = teamMemberDiv.getTextFromClass("name")
-          val nationality = node \ "span" \@ "title"
-          Player(name, number.toInt, nationality)
-        }
-      case MatchTeamMemberTags.CoachTag  =>
-        members.map { node =>
-          val name = (node \ "div").getTextFromClass("name")
-          val nationality = node \ "span" \@ "title"
-          Coach(name, nationality)
-        }
+  private def defineNextTeamMembers(curr: Node): MatchTeamMemberTag = {
+    curr.text.withoutWhitespacesAtFrontAndBack match {
+      case "Starting Lineups" => MatchTeamMemberTags.StartingLineupPlayerTag
+      case "Substitutes" => MatchTeamMemberTags.PlayerOnTheBenchTag
+      case "Coaches" => MatchTeamMemberTags.CoachTag
     }
   }
 
-  private def allLineups(matchLineupsAsHtml: Elem): Map[MatchTeamMemberTag, NodeSeq] = {
-    val allEventsData = (matchLineupsAsHtml \\ "tr").toVector
-    val allEventsDataSize = allEventsData.size
-    val startEventIndexes = findMatchStageBeginningIndexes(allEventsData).withDefaultValue(allEventsDataSize)
-    val startingLineupPlayersNodes = allEventsData.slice(startEventIndexes(MatchTeamMemberTags.StartingLineupPlayerTag), startEventIndexes(MatchTeamMemberTags.PlayerOnTheBenchTag))
-    val playersOnTheBenchNodes = allEventsData.slice(startEventIndexes(MatchTeamMemberTags.PlayerOnTheBenchTag), startEventIndexes(MatchTeamMemberTags.CoachTag))
-    val coachNodes = allEventsData.slice(startEventIndexes(MatchTeamMemberTags.CoachTag), allEventsDataSize)
-    Map(
-      MatchTeamMemberTags.StartingLineupPlayerTag -> onlyTimedEvents(startingLineupPlayersNodes),
-      MatchTeamMemberTags.PlayerOnTheBenchTag -> onlyTimedEvents(playersOnTheBenchNodes),
-      MatchTeamMemberTags.CoachTag -> onlyTimedEvents(coachNodes)
-    )
+  private def extractMembersFromNode(curr: Node, currentTag: MatchTeamMemberTag): List[ParsedTeamMember] = {
+    val currTeamMemberTds = onlyTimedEvents(curr).lift
+    val firstMemberParsed = currTeamMemberTds(0).map(parseTeamMember(currentTag, _))
+    val secondMemberParsed = currTeamMemberTds(1).map(parseTeamMember(currentTag, _))
+    List(firstMemberParsed, secondMemberParsed).flatten
   }
 
-  private def onlyTimedEvents(events: NodeSeq) = {
+  private def onlyTimedEvents(events: NodeSeq): NodeSeq = {
     (events \\ "td").filter { tableCell =>
       val potentiallyTimedEvents = (tableCell \\ "div").map(_ \@ "class")
       potentiallyTimedEvents.containsElemWithWord("time-box")
     }
   }
 
-  private def findMatchStageBeginningIndexes(allTeamMembersData: Vector[Node]): Map[MatchTeamMemberTag, Int] = {
-    allTeamMembersData.zipWithIndex.collect { case (node, i) if (node \@ "class").isEmpty =>
-      node.text.withoutWhitespacesAtFrontAndBack match {
-        case "Starting Lineups" => (MatchTeamMemberTags.StartingLineupPlayerTag, i)
-        case "Substitutes" => (MatchTeamMemberTags.PlayerOnTheBenchTag, i)
-        case "Coaches" => (MatchTeamMemberTags.CoachTag, i)
-      }
-    }.toMap
+  private def parseTeamMember(matchTag: MatchTeamMemberTag, currTd: Node): ParsedTeamMember = {
+    val matchTeamTag = homeOrAway(currTd)
+    matchTag match {
+      case playerTeamMemberTag: PlayerTeamMemberTag =>
+        val player = parsePlayer(currTd)
+        ParsedTeamMember(matchTeamTag, (player, playerTeamMemberTag).left)
+      case MatchTeamMemberTags.CoachTag =>
+        val coach = parseCoach(currTd)
+        ParsedTeamMember(matchTeamTag, coach.right)
+    }
   }
 
-  private def groupEventsByTeam(matchEvents: Map[MatchTeamMemberTag, NodeSeq]): Map[MatchTeamTag, Map[MatchTeamMemberTag, NodeSeq]] = {
-    val groupedByStage = groupEventsByStage(matchEvents)
-    val sequenced = groupedByStage.sequence
-    sequenced.withDefaultValue(Map())
+  private def parsePlayer(node: Node): Player = {
+    val teamMemberDiv = node \ "div"
+    val number = teamMemberDiv.getTextFromClass("time-box")
+    val name = teamMemberDiv.getTextFromClass("name")
+    val nationality = node \ "span" \@ "title"
+    Player(name, number.toInt, nationality)
   }
 
-  private def groupEventsByStage(matchEvents: Map[MatchTeamMemberTag, NodeSeq]): Map[MatchTeamMemberTag, Map[MatchTeamTag, NodeSeq]] = {
-    matchEvents.mapValues { events => events.groupBy { event =>
-      val eventType = event \@ "class"
-      val eventTypeMark = eventType.takeRight(2)
-
-      eventTypeMark match {
-        case `homeTeamMark` => HomeTeam
-        case `awayTeamMark` => AwayTeam
-      }
-    }}
+  private def parseCoach(node: Node): Coach = {
+    val name = (node \ "div").getTextFromClass("name")
+    val nationality = node \ "span" \@ "title"
+    Coach(name, nationality)
   }
 
+  private def homeOrAway(event: Node): MatchTeamTag = {
+    val eventType = event \@ "class"
+    val eventTypeMark = eventType.takeRight(2)
+    eventTypeMark match {
+      case `homeTeamMark` => HomeTeam
+      case `awayTeamMark` => AwayTeam
+    }
+  }
 
+  private def extractParsedMembers(homeMembers: List[ParsedTeamMember]): TeamLineup = {
+    val startingPlayers = homeMembers.collect { case ParsedTeamMember(_, -\/((player, MatchTeamMemberTags.StartingLineupPlayerTag))) => player }
+    val bench = homeMembers.collect { case ParsedTeamMember(_, -\/((player, MatchTeamMemberTags.PlayerOnTheBenchTag))) => player }
+    val coach = homeMembers.collectFirst { case ParsedTeamMember(_, \/-(c)) => c }
+    TeamLineup(startingPlayers, bench, coach)
+  }
 }
